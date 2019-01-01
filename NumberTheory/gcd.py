72d1f8af80e249b9473b86b28035eb3050bea688
mishrasunny174 <mishrasunny174@gmail.com>
2019-01-01 13:08:56 +0530

    changed from recursive to iterative

--- NumberTheory/gcd.py
+++ NumberTheory/gcd.py
@@ -1,11 +1,10 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 import sys
 
 def gcd(a,b):
-	if a%b==0:
-		return b
-	else:
-		return gcd(b,a%b)
+	while a%b != 0:
+		a,b = b, a%b
+	return b	
 
 def main():
 	if len(sys.argv) != 3:
