c47190a65aeffbe84650084552e62ecf9ea6cc2f
Sunny Mishra <mishrasunny174@gmail.com>
2019-12-20 22:37:35 +0530

    implemented a simple canary

---
+++ NetworkProgramming/honeypot/app.py
@@ -0,0 +1,37 @@
+import threading
+import socket
+import logging
+
+HOST = "0.0.0.0"
+threads = []
+stop = False
+
+def handle(client, addr, port):
+	logger = logging.getLogger()
+	# print(addr[0],":",addr[1])
+	logger.warning("Connect from {} on {}".format(addr[0], port))
+	client.close()
+
+
+def serve(port):
+	servSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	servSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	servSock.bind((HOST, port))
+	servSock.listen(10)
+	global stop
+	while not stop:
+		clientSock, clientAddr = servSock.accept()
+		threading.Thread(target=handle, args=(clientSock, clientAddr, port)).start()
+
+def honeypot(ports):
+	for port in ports:
+		threads.append(threading.Thread(target=serve, args=(port,)))
+	for t in threads:
+		t.start()
+
+def main():
+	logging.basicConfig(filename="connections.log", format="%(asctime)s %(message)s")
+	honeypot([80, 22, 8080, 2222,3333,4444,5555])
+
+if __name__ == "__main__":
+	main()
